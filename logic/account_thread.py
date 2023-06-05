b2021fc04cbcf4237a62ca61bca7d2e2ad0a1e46
cppmyk <molockh@protonmail.com>
2023-06-05 14:41:40 +0200

    Add btcb mode running method

--- logic/account_thread.py
+++ logic/account_thread.py
@@ -10,6 +10,7 @@ from base.errors import BaseError
 from config import TimeRanges, BridgerMode, RefuelMode
 from logger import setup_thread_logger
 from logic.stargate_states import SleepBeforeStartStargateBridgerState, CheckStablecoinBalanceState
+from logic.btcb_states import SleepBeforeStartBTCBridgerState, CheckBTCbBalanceState
 from logic.state import InitialState
 
 logger = logging.getLogger(__name__)
@@ -31,11 +32,14 @@ class AccountThread(threading.Thread):
         if self.bridger_mode == BridgerMode.STARGATE:
             self._run_stargate_mode()
         elif self.bridger_mode == BridgerMode.BTCB:
-            pass
+            self._run_btcb_mode()
         elif self.bridger_mode == BridgerMode.TESTNET:
             pass
         else:
             raise ValueError("Unknown BridgeMode")
+
+    def set_state(self, state):
+        self.state = state
 
     def _run_stargate_mode(self):
         logger.info("Running Stargate bridger")
@@ -60,5 +64,25 @@ class AccountThread(threading.Thread):
                 self.set_state(CheckStablecoinBalanceState())
                 time.sleep(10 * TimeRanges.MINUTE)
 
-    def set_state(self, state):
-        self.state = state
+    def _run_btcb_mode(self):
+        logger.info("Running BTC.b bridger")
+
+        self.set_state(SleepBeforeStartBTCBridgerState())
+        while True:
+            try:
+                self.state.handle(self)
+            except BaseError as ex:
+                logger.error(f'Exception: {ex}')
+                self.set_state(CheckBTCbBalanceState())
+            except requests.exceptions.HTTPError:
+                logger.error(f'Too many request to HTTP Provider!')
+                self.set_state(CheckBTCbBalanceState())
+                time.sleep(TimeRanges.MINUTE)
+            except RateLimitExceeded:
+                logger.error(f'Too many request to exchange!')
+                self.set_state(CheckBTCbBalanceState())
+                time.sleep(TimeRanges.MINUTE)
+            except InsufficientFunds:
+                logger.error(f'Not enough balance on exchange!')
+                self.set_state(CheckBTCbBalanceState())
+                time.sleep(10 * TimeRanges.MINUTE)
